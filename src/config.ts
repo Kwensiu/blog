b03b32fea6a89ef17e651e0e76cd3f4a753b17cb
Kwensiu <kwen.siu@gmail.com>
2025-12-18 00:35:05 +0800

    feat(navbar): 更新Navbar样式并优化动画效果
    - 将 `#navbar` 的类名从 `onload-animation` 更改为 `navbar-slide`，并添加滑入动画
    - 更新Navbar背景样式，增加毛玻璃效果与边框
    - 调整 `.float-panel` 圆角大小，并新增 `.navbar` 样式定义
    - 在 Tailwind 配置中扩展 backdrop blur 和高度工具类
    - 修改站点标题
    - 移除文章页底部mb-4

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Ciallo",
+	title: "ヾ(•ω•`)o",
 	subtitle: "ollaiƆ",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,20 @@ module.exports = {
       fontFamily: {
         sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
+      backdropBlur: {
+        xs: '2px',
+        lg: '40px',
+      },
+      // 添加backdropBlur相关功能
+      backdropFilter: {
+        'none': 'none',
+        'blur': 'blur(12px)',
+      },
+      height: {
+        '17': '4.25rem',  // 68px
+        '18': '4.5rem',   // 72px
+        '19': '4.75rem',  // 76px
+      }
     },
   },
   plugins: [require("@tailwindcss/typography")],
